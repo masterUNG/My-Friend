bff584405298413f3abf874f4e984d5cf40d773d
masterUNG <phrombutr@gmail.com>
2016-09-17 13:57:39 +0700

    neae

--- app/src/main/java/appewtc/masterung/myfriend/SignUpActivity.java
+++ app/src/main/java/appewtc/masterung/myfriend/SignUpActivity.java
@@ -1,16 +1,32 @@
 package appewtc.masterung.myfriend;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText nameEditText, userEditText,
+            passwordEditText, rePasswordEditText;
+    private RadioGroup radioGroup;
+    private RadioButton maleRadioButton, femaleRadioButton;
+    private ImageView imageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-    }   // Main Method
 
+        //Bind Widget
+
+
+
+    }   // Main Method
 
 
 }   // Main Class
